64865507175b9e762bf4303dc92d2d70dd605810
Anubhav Ashish <anubhav.ashish9@gmail.com>
2022-10-03 14:16:07 +1100

    bugFix: Due to case mismatch react was throwing errors for Login and sign up form.

---
+++ group-y/src/formHandlers/LogInForm.js
@@ -0,0 +1,46 @@
+//Imports
+import React, { useState } from "react";
+import Stack from '@mui/material/Stack';
+import Button from '@mui/material/Button';
+
+//Need A button that says register to go to a register page
+//instead of the initial login page
+const LogInForm = ({updateFn}) => {
+
+    const initialState = {username: '', password: ''}
+
+    const [formInfo, setFormInfo] = useState(initialState)
+
+    const updateField = (event) => {
+        // which input element is this
+        const name = event.target.attributes.name.value
+        if (name === "username") {
+            setFormInfo({...formInfo, username: event.target.value})
+        } else if (name === "password") {
+            setFormInfo({...formInfo, password: event.target.value})
+        }
+    }
+
+    const formHandler = (event) => {
+        event.preventDefault()
+        console.log("Form submitted: ", formInfo)
+        updateFn(formInfo)
+        // setFormInfo(initialState)
+    }
+
+    return (
+        <div className ="formContainer">
+            <form className="border" onSubmit={formHandler}>
+                <label className="label" htmlFor="username">Username</label>
+                <input className="input" type="text" placeholder="Enter your username" name="username" onChange={updateField} required/>
+                <label className="label" htmlFor="password">Password</label>
+                <input className="input" type="password" placeholder="Enter your password" name="password" onChange={updateField} required/>
+                <Stack spacing={2} alignItems="center">
+                    <Button type="submit" variant="contained">Login</Button>
+                </Stack>
+            </form>
+        </div>
+    )
+}
+
+export default LogInForm
